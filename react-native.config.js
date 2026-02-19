008dff6160e09fa792cfda525fe6bbf9e9ef1ce0
Uktam Khamidov <uktam.kh@maydongroup.com>
2026-02-19 10:07:17 +0500

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -5,9 +5,7 @@ module.exports = {
         sourceDir: './android',
         packageImportPath: 'import com.reactnativemyid.MyIdPackage;',
       },
-      ios: {
-        podspecPath: './react-native-myid.podspec',
-      },
+      ios: {},
     },
   },
 };
